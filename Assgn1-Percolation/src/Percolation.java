972f03a8d7f5fb300373e8b4dcf4bfcd6288877b
Mandeep Condle <mandeepcondle@gmail.com>
2014-02-17 16:07:10 -0500

    adding readme files

--- Assgn1-Percolation/src/Percolation.java
+++ Assgn1-Percolation/src/Percolation.java
@@ -1,37 +1,78 @@
 
 
 public class Percolation {
-    public static final int CLOSED = 1;
+
+    public static final int CLOSED = 0;
     public static final int OPEN   = 1;
-    public static final int FULL   = 1;
 
 	private int[][] sites;
+    private int N;
+    private UF uf;
 
+    /**
+     * Creates an N-by-N grid with all sites blocked
+     * @param N grid side
+     */
     public Percolation(int N) {
-    	this.sites = new int[N][N];
-    	for (int i=0; i<N; i++) {
-    		for (int j=0; j<N; j++) {
+        this.N = N+1;
+        this.uf = new UF(this.N * this.N);
+    	this.sites = new int[this.N][this.N];
+
+    	for (int i=1; i<this.N; i++) {
+    		for (int j=0; j<this.N; j++) {
     			this.sites[i][j] = CLOSED;
     		}
     	}
     }
 
+    /**
+     * Opens site(i,j) if not opened already
+     * @param i row
+     * @param j col
+     */
     public void open(int i, int j) {
-    	if (this.sites[i][j] != OPEN) {
+    	if (i<1 || j<1 || i>this.N || j>this.N) throw new IndexOutOfBoundsException();
+        if (this.sites[i][j] == CLOSED) {
     		this.sites[i][j] = OPEN;
     	}
     }
 
+    /**
+     * Is site(i,j) open?
+     * @param i row
+     * @param j col
+     * @return  T/F
+     */
     public boolean isOpen(int i, int j) {
+        if (i<1 || j<1 || i>this.N || j>this.N) throw new IndexOutOfBoundsException();
 		return this.sites[i][j] == OPEN;
     }
 
+    /**
+     * Is site(i,j) full?
+     * @param i row
+     * @param j col
+     * @return  T/F
+     */
     public boolean isFull(int i, int j) {
-        return this.sites[i][j] == FULL;
+        if (i<1 || j<1 || i>this.N || j>this.N) throw new IndexOutOfBoundsException();
+
+        //For (i,j) to be full, it should be open and connected to an open site on the first row
+        int n = (N-1)*i + j;
+
+        if (this.isOpen(i, j) && (uf.find(n) >= 1 && uf.find(n) <= this.N )) {
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Computes if the system percolates
+     * @return  T/F
+     */
     public boolean percolates() {
-    	UF uf = new UF(10); //sample test for UF
+    	UF uf = new UF(this.N * this.N); //sample test for UF
+
         uf.union(2, 5);
 
     	return false;
--- Assgn1-Percolation/src/PercolationStats.java
+++ Assgn1-Percolation/src/PercolationStats.java
@@ -1,22 +1,49 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
 
 public class PercolationStats {
 
+    /**
+     * Perform T independent computational experiments on an N-by-N grid
+     * @param N grid side
+     * @param T # of computations
+     */
     public PercolationStats(int N, int T) {
         if (N <= 0 || T <= 0) throw new IllegalArgumentException();
 
 
     }
 
+    /**
+     * Computes sample mean of percolation threshold
+     * @return  mean
+     */
     public double mean() {
 
         return 0.0;
     }
 
+    /**
+     * Computes standard deviation of perocolation threshold
+     * @return  SD
+     */
     public double stddev() {
         return 0.0;
     }
 
-    public double confidenceLow() {
+    /**
+     * Returns lower bound of the 95% confidence interval
+     * @return lower bound
+     */
+    public double confidenceLo() {
+        return 0.0;
+    }
+
+    /**
+     * Returns upper bound of the 95% confidence interval
+     * @return upper bound
+     */
+    public double confidenceHi() {
         return 0.0;
     }
 
@@ -25,6 +52,9 @@ public class PercolationStats {
      * @param args
      */
     public static void main(String[] args) {
+
+        System.out.println();
+
 
     }
 
